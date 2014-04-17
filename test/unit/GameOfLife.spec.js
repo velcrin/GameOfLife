8340301a04cf09b5ff261dddd501dbf9e71f9d96
Vincent <vincent.elcrin@gmail.com>
2014-04-17 21:32:57 +0200

    A dead cell lives when at the next generation when three of it surrounding neighbours are alive

--- test/unit/GameOfLife.spec.js
+++ test/unit/GameOfLife.spec.js
@@ -1,50 +1,7 @@
 chai.Should();
 
-var GameOfLife = (function() {
-    var ALIVE = "alive", DEAD = "dead";
-
-    function Cell(initialState) {
-        var state = initialState || DEAD;
-
-        function getNumberOfAlive(neighbours) {
-            var nbAliveCells = 0;
-            neighbours.forEach(function (neighbor) {
-                if (neighbor.isAlive()) {
-                    nbAliveCells += 1;
-                }
-            });
-            return nbAliveCells;
-        }
-
-        return {
-            calculateState: function (neighbours) {
-                var liveCellNumber = getNumberOfAlive(neighbours);
-                if(liveCellNumber < 2) {
-                    state = DEAD;
-                }
-                if(liveCellNumber > 3) {
-                    state = DEAD;
-                }
-            },
-            isAlive: function() {
-                return state === ALIVE;
-            }
-        };
-    }
-
-    function AliveCell() {
-        return new Cell(ALIVE);
-    }
-
-    function DeadCell() {
-        return new Cell(DEAD);
-    }
-
-    return {
-        AliveCell: AliveCell
-    };
-})(),
-    AliveCell = GameOfLife.AliveCell;
+var AliveCell = GameOfLife.AliveCell,
+    DeadCell = GameOfLife.DeadCell;
 
 describe('Live cell', function () {
     it('should die when it has fewer than two live neighbours', function () {
@@ -73,5 +30,36 @@ describe('Live cell', function () {
             new AliveCell()]);
 
         cell.isAlive().should.be.false;
+    });
+    it('should live when it has two living neighbours', function () {
+        var cell = new AliveCell();
+
+        cell.calculateState([
+            new AliveCell(),
+            new AliveCell()]);
+
+        cell.isAlive().should.be.true;
+    });
+    it('should live when it has three living neighbours', function () {
+        var cell = new AliveCell();
+
+        cell.calculateState([
+            new AliveCell(),
+            new AliveCell(),
+            new AliveCell()]);
+
+        cell.isAlive().should.be.true;
+    });
+});
+describe('Dead cell', function () {
+    it('should live when it has three living neighbours', function () {
+        var cell = new DeadCell();
+
+        cell.calculateState([
+            new AliveCell(),
+            new AliveCell(),
+            new AliveCell()]);
+
+        cell.isAlive().should.be.true;
     });
 });
